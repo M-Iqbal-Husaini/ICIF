56dffffc689ba3e00a04aee0afb892fe7ca94ed6
M-Iqbal-Husaini <dovikaloo262@gmail.com>
2024-11-27 20:03:01 +0700

    Modul 4_Laravel 11

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,31 +6,42 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Distributor;
 use Illuminate\Http\Request;
-use App\Models\Product;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\DB;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
+        $data = DB::table('distributors')
+        ->join('products', 'distributors.id', '=', 'products.id_distributor')
+        ->select('distributors.*', 'products.*')
+        ->get();
 
-        return view('pages.admin.product.index', compact('products'));
+        confirmDelete('Hapus Data!', 'Apakah anda yakin ingin menghapus data ini?');
+
+        return view('pages.admin.product.index', compact('data'));
     }
 
     public function create()
     {
-        return view('pages.admin.product.create');
+        $distributor = Distributor::all();
+
+        return view('pages.admin.product.create', compact('distributor'));
     }
 
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
+            'id_distributor' => 'required|numeric',
             'name' => 'required',
             'price' => 'numeric|required',
             'category' => 'required',
             'description' => 'required',
-            'image' => 'required|mimes:png,jpeg,jpg',
-            'discount' => 'nullable|numeric|min:0|max:100', // Discount validation
+            'image' => 'required|image|mimes:PNG,png,jpeg,jpg',
+            'discount' => 'nullable|numeric|min:0|max:100',
         ]);
 
         if ($validator->fails()) {
@@ -45,12 +56,13 @@ class ProductController extends Controller
         }
 
         $product = Product::create([
+            'id_distributor' => $request->id_distributor,
             'name' => $request->name,
             'price' => $request->price,
             'category' => $request->category,
             'description' => $request->description,
             'image' => $imageName,
-            'discount' => $request->discount ?? 0, // Set discount or default to 0
+            'discount' => $request->discount ?? 0,
         ]);
 
         if ($product) {
@@ -64,31 +76,33 @@ class ProductController extends Controller
 
     public function detail($id)
     {
-        // Find the product by its ID
-        $product = Product::findOrFail($id);
+        $data = DB::table('distributors')
+        ->join('products', 'distributors.id', '=', 'products.id_distributor')
+        ->select('products.*', 'distributors.*')
+        ->where('products.id', '=', $id)
+        ->first();
 
-        // Return the view with the product details
-        return view('pages.admin.product.detail', compact('product'));
+        return view('pages.admin.product.detail', compact('data'));
     }
-
-
 
     public function edit($id)
     {
         $product = Product::findOrFail($id);
+        $distributor = Distributor::all();
 
-        return view('pages.admin.product.edit', compact('product'));
+        return view('pages.admin.product.edit', compact('product', 'distributor'));
     }
 
     public function update(Request $request, $id)
     {
         $validator = Validator::make($request->all(), [
+            'id_distributor' => 'required|numeric',
             'name' => 'required',
             'price' => 'numeric|required',
             'category' => 'required',
             'description' => 'required',
             'image' => 'nullable|mimes:png,jpeg,jpg',
-            'discount' => 'nullable|numeric|min:0|max:100', // Discount validation
+            'discount' => 'nullable|numeric|min:0|max:100',
         ]);
 
         if ($validator->fails()) {
@@ -112,12 +126,13 @@ class ProductController extends Controller
         }
 
         $product->update([
+            'id_distributor' => $request->id_distributor,
             'name' => $request->name,
             'price' => $request->price,
             'category' => $request->category,
             'description' => $request->description,
             'image' => $imageName,
-            'discount' => $request->discount ?? 0, // Update discount
+            'discount' => $request->discount ?? 0,
         ]);
 
         if ($product) {
@@ -131,13 +146,8 @@ class ProductController extends Controller
 
     public function delete($id)
     {
+
         $product = Product::findOrFail($id);
-
-        $oldPath = public_path('images/' . $product->image);
-        if (File::exists($oldPath)) {
-            File::delete($oldPath);
-        }
-
         $product->delete();
 
         if ($product) {
